55da4aaa3b5c545f1e6b128ad5e5c833523a043f
Lilyana Petkova <lpetkova@living-group.com>
2022-03-21 12:40:38 +0200

    inital commit

---
+++ Package/js/lp.checkboxlist.controller.js
@@ -0,0 +1,92 @@
+﻿angular.module("umbraco")
+    .controller("LP.Checkbox.Controller",
+        function ($scope, $http, $routeParams) {
+            var newItems = [];
+            $scope.selectedItems = [];
+            $scope.selectedItemsSortOrder = [];
+            var items = function () {
+                return $http.get($scope.model.config.apiUrl, { params: { id: $routeParams.id } })
+                    .then(function (data) {
+                        $scope.items = data;
+                        for (var i = 0; i < $scope.items.data.length; i++) {
+                            newItems.push({ id: $scope.items.data[i].id, value: $scope.items.data[i].name });
+                        }
+                        $scope.items = newItems;
+                    });
+            }
+            items()
+                .then(function () {
+                    $scope.$on("formSubmitting",
+                        function (ev, args) {
+
+                            var selectedItems = [];
+                            angular.forEach($scope.selectedItems,
+                                function (value, key) {
+                                    var itemSelected = value.selected;
+                                    if (itemSelected === true) {
+                                        selectedItems.push({
+                                            value: value.key,
+                                            name: value.val,
+                                            sortOrder: value.sortOrder,
+                                            selected: value.selected
+                                        });
+                                    }
+                                });
+                            $scope.model.value = selectedItems;
+                        });
+
+                    function setupViewModel() {
+                        if ($scope.model.value === null || $scope.model.value === undefined) {
+                            $scope.model.value = [];
+                        }
+                        var sortOrder = 1;
+                        for (var i = 0; i < $scope.items.length; i++) {
+
+                            var isSelected = false;
+                            if ($scope.model.value !== null && $scope.model.value !== undefined && $scope.model.value !== [] ) {
+                                for (var k = 0; k < $scope.model.value.length; k++) {
+                                    if ($scope.model.value[k].value.toString() === $scope.items[i].id.toString()) {
+                                      
+                                        sortOrder = $scope.model.value[k].sortOrder;
+                                        isSelected = true;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            $scope.selectedItems.push({
+                                selected: isSelected,
+                                sortOrder: sortOrder,
+                                key: $scope.items[i].id,
+                                val: $scope.items[i].value
+                            });
+                            sortOrder++;
+                        }
+                    }
+
+                    setupViewModel();
+
+                    $scope.$watch("selectedItems",
+                        function (newVal, oldVal) {
+                            $scope.model.value = [];
+
+                            for (var x = 0; x < $scope.selectedItems.length; x++) {
+                                if ($scope.selectedItems[x].selected) {
+                                    $scope.model.value.push({
+                                        selected: $scope.selectedItems[x].selected,
+                                        value: $scope.selectedItems[x].key,
+                                        name: $scope.selectedItems[x].value,
+                                        sortOrder: $scope.selectedItems[x].sortOrder
+                                    });
+                                }
+                            }
+                        },
+                        true);
+
+
+                    $scope.model.onValueChanged = function (newVal, oldVal) {
+
+                        setupViewModel();
+                    };
+                });
+        });
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # LP-Checkbox
-Custom Umbraco Checkbox 
+Custom Umbraco Checkbox with sort order
+
+This field renders a list of checkbox items from the site which are related to the current page the field is on.
+
+When creating a field of this type, it is asking you for the api url to retrieve the data from :
+api/{controller}/{action}
+
+The id of the current page is retrieved from the route.
